a8771027b39a24233ebc859700f22ddda2bb0079
Samuel Keating-Fry <samuelkf@users.noreply.github.com>
2016-01-25 16:13:38 +0000

    submit form via ajax and make vodoafone site request

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'haml'
+require 'json'
+require 'mechanize'
 
 get '/styles.css' do 
   scss :styles
@@ -8,4 +10,29 @@ end
 get '/' do
   @title = 'page me'
   haml :form
+end
+
+post '/sendmessage' do
+  content_type :json
+  return_message = {}
+
+  agent = Mechanize.new
+  agent.user_agent_alias = 'Windows IE 11'
+
+  page = agent.get ('http://www.paging.vodafone.net/login.jsp')
+  form1 = page.form('formSendMessage1')
+  form1.to_string = "***REMOVED***"
+  page = agent.submit(form1)
+  form2 = page.form('sendmessage')
+  form2.mesg_to_send = params[:message_text].to_s
+  page = agent.submit(form2)
+
+  if page.search("Message successfully sent")
+    return_message[:status] = 'success'
+  else
+    status 502
+    return_message[:status] = 'failed'
+  end
+
+  return_message.to_json
 end
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,7 +1,27 @@
 $(document).ready(function(){
   var characters = 240;
-  $("#message").keyup(function(){
+  $(".message_text").keyup(function(){
     var remaining = characters -  $(this).val().length;
     $(".counter").html(remaining);
   });
+
+  $(function() {
+  $("form.message").submit(function(e){
+    e.preventDefault();
+
+    $(this).hide();
+
+    $.ajax({
+      type: $(this).attr("method"),
+      url: $(this).attr("action"),
+      data: $('form.register').serialize(),
+      success: function(){
+        $(".form_container").append("<p>message sent</p>")
+      },
+      error: function(){
+        $(".form_container").append("<p>failed</p>")
+      }
+    });
+  });
+});
 });
