16017e1afcca7de8404a04ff9b272374855bca38
Samuel Keating-Fry <sam@hyper.xxx>
2016-02-27 16:40:21 +0000

    log messages to google, make API requests in new thread

--- app.rb
+++ app.rb
@@ -14,33 +14,59 @@ get '/' do
 end
 
 post '/sendmessage' do
-  content_type :json
-  return_message = {}
 
+  # Create an instance of Mechanize and set the UA string.
   agent = Mechanize.new
   agent.user_agent_alias = 'Windows IE 11'
 
+  # Enter the pager number into the first form and submit
   page = agent.get ('http://www.paging.vodafone.net/login.jsp')
   form1 = page.form('formSendMessage1')
   form1.to_string = "***REMOVED***"
   page = agent.submit(form1)
+
+  # Enter the message into the second page and submit
   form2 = page.form('sendmessage')
   form2.mesg_to_send = params[:message_text].to_s
   page = agent.submit(form2)
 
+  # Return a success/failure status based on the content of Vodafone's confirmation page
+  content_type :json
+  return_message = {}
+
   if page.search("Message successfully sent")
+
     return_message[:status] = 'success'
-    url = URI.parse("https://api.pushover.net/1/messages.json")
-    req = Net::HTTP::Post.new(url.path)
-    req.set_form_data({
-      :token => "***REMOVED***",
-      :user => "***REMOVED***",
-      :message => params[:message_text].to_s,
-    })
-    res = Net::HTTP.new(url.host, url.port)
-    res.use_ssl = true
-    res.verify_mode = OpenSSL::SSL::VERIFY_PEER
-    res.start {|http| http.request(req) }
+
+    # Make external API calls in a new thread
+    Thread.new do
+
+      # Send a notification using Pushover
+      url = URI.parse("https://api.pushover.net/1/messages.json")
+      req = Net::HTTP::Post.new(url.path)
+      req.set_form_data({
+        :token => "***REMOVED***",
+        :user => "***REMOVED***",
+        :message => params[:message_text].to_s,
+      })
+      res = Net::HTTP.new(url.host, url.port)
+      res.use_ssl = true
+      res.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      res.start {|http| http.request(req) }
+
+      # Trigger an IFTTT event
+      url = URI.parse("https://maker.ifttt.com/trigger/pageme_message_sent/with/key/***REMOVED***")
+      req = Net::HTTP::Post.new(url.path)
+      req.set_form_data({
+        :value1 => params[:message_text].to_s,
+        :value2 => request.ip
+      })
+      res = Net::HTTP.new(url.host, url.port)
+      res.use_ssl = true
+      res.verify_mode = OpenSSL::SSL::VERIFY_PEER
+      res.start {|http| http.request(req) }
+    end
+
   else
     status 502
     return_message[:status] = 'failed'
